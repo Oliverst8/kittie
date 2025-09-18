2c0ff807a35b1fed9b23e37aa1a72803010cf052
Oliver Starup <Oliver@starup99.dk>
2025-09-18 15:41:35 +0200

    Added constest module

---
+++ src/commands/contest.rs
@@ -0,0 +1,4 @@
+pub async fn contest(app: &App, args: &GetArgs) -> crate::Result<()> {
+    println!("Contest called");
+    Ok(())
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,4 +1,5 @@
 mod config;
+mod contest;
 mod debug;
 mod get;
 mod langs;
@@ -8,6 +9,7 @@ mod test;
 mod update;
 
 pub use config::config;
+pub use contest::contest;
 pub use debug::debug;
 pub use get::get;
 pub use langs::langs;
